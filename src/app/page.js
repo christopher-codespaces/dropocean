440f03e34f40c1a35599e7b57f2767c6343f9be5
christopher-codespaces <christopher.swiftlybox@gmail.com>
2024-08-15 19:01:58 +0200

    SEO /Heatmap

--- src/app/page.js
+++ src/app/page.js
@@ -6,37 +6,43 @@ import { Services } from "../components/OurSolutions";
 import { OurProcess } from "../components/OurProcess";
 import { CrazyHomeFooter } from "../components/CrazyHomeFooter";
 import Portfolio from "../components/Portfolio";
+import { Metadata } from "next";
 
+
+export const metadata = {
+  title: "The Best Web Design Agency In New York | Cape Neto Solutions ",
+  description:
+    "Cape Neto Design specializes in top-tier web design and development for the construction industry, offering advanced solutions such as booking systems, automation, and SEO optimization. Our expertise ensures that construction businesses gets high converting leads for your business, high-performance websites.",
+  keywords:
+    "Web design, construction websites, booking systems, automation, SEO, web development",
+  openGraph: {
+    title: "Cape Neto Design | Home",
+    description:
+      "Cape Neto Design provides top-notch web design and development services for the construction industry, focusing on advanced solutions like booking systems, automation, and SEO optimization.",
+    url: "https://www.capeneto.com",
+    images: [
+      {
+        url: "/Macbook-Air-peachtreepools.com.png",
+        width: 1200,
+        height: 630,
+        alt: "Cape Neto Design Open Graph Image",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Cape Neto Design ",
+    description:
+      "Cape Neto Design specializes in web design and development for the construction industry, offering services such as advanced booking systems, automation, and SEO optimization.",
+    image: "/ui.png",
+  },
+  canonical: "https://www.capeneto.net",
+};;
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Cape Neto Solutions | Home</title>
-        <meta
-          name="description"
-          content="Cape Neto Design offers exceptional web design and development services specializing in construction industry solutions. Our expertise includes advanced booking systems, automation, SEO, and more."
-        />
-        <meta
-          name="keywords"
-          content="Web design, construction websites, booking systems, automation, SEO, web development"
-        />
-        <meta property="og:title" content="Cape Neto Design | Home" />
-        <meta
-          property="og:description"
-          content="Cape Neto Design provides top-notch web design and development services for the construction industry, focusing on advanced solutions like booking systems, automation, and SEO optimization."
-        />
-        <meta property="og:image" content="/path/to/your/og-image.jpg" />
-        <meta property="og:url" content="https://www.yourwebsite.com" />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Cape Neto Design | Home" />
-        <meta
-          name="twitter:description"
-          content="Cape Neto Design specializes in web design and development for the construction industry, offering services such as advanced booking systems, automation, and SEO optimization."
-        />
-        <meta name="twitter:image" content="/path/to/your/twitter-image.jpg" />
-        <link rel="canonical" href="https://www.capeneto.net" />
-      </Head>
+      
       <Hero />
       <Review />
       <Services />
--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,12 +1,44 @@
 import React from "react";
 import Projects from "../../components/Projects";
+import { Metadata } from "next";
 
+
+export const metadata = {
+  title: "The Best Web Design Agency In New York | Our Work ",
+  description:
+    "Cape Neto Design specializes in top-tier web design and development for the construction industry, offering advanced solutions such as booking systems, automation, and SEO optimization. Our expertise ensures that construction businesses gets high converting leads for your business, high-performance websites.",
+  keywords:
+    "Web design, construction websites, booking systems, automation, SEO, web development",
+  openGraph: {
+    title: "Cape Neto Design | Home",
+    description:
+      "Cape Neto Design provides top-notch web design and development services for the construction industry, focusing on advanced solutions like booking systems, automation, and SEO optimization.",
+    url: "https://www.capeneto.com",
+    images: [
+      {
+        url: "/Macbook-Air-peachtreepools.com.png",
+        width: 1200,
+        height: 630,
+        alt: "Cape Neto Design Open Graph Image",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Cape Neto Design ",
+    description:
+      "Cape Neto Design specializes in web design and development for the construction industry, offering services such as advanced booking systems, automation, and SEO optimization.",
+    image: "/ui.png",
+  },
+  canonical: "https://www.capeneto.net",
+};;
 function page() {
   return (
-    <>
+    <div>
       <Projects />
       
-    </>
+    </div>
   );
 }
 
